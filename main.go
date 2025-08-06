6fd872bf684b4e8fb62b30e4c8e8f20fb55568de
Phillip Theriault <phillip.theriault@spellweaver.com>
2025-08-05 18:56:47 -0600

    Added health check and air

--- main.go
+++ main.go
@@ -9,22 +9,31 @@ func main() {
     const port = "8080"
     const root = "./www"
 
+    // https://pkg.go.dev/net/http#FileServer
+    fs := http.FileServer(http.Dir(root))
+
     // https://pkg.go.dev/net/http#ServeMux
     mux := http.NewServeMux()
 
+    // https://pkg.go.dev/net/http#ServeMux.Handle
+    //mux.Handle("/", fs)
+    mux.Handle("/app/", http.StripPrefix("/app", fs))
+    mux.HandleFunc("/healthz", checkHealth)
+
     // https://pkg.go.dev/net/http#Server
+    // http.Server is a struct that defines the server configuration
     server := http.Server {
         Addr: ":" + port,
         Handler: mux,
     }
 
-    // https://pkg.go.dev/net/http#FileServer
-    fs := http.FileServer(http.Dir(root))
-
-    // https://pkg.go.dev/net/http#ServeMux.Handle
-    mux.Handle("/", fs)
-
     // https://pkg.go.dev/net/http#Server.ListenAndServe
     log.Printf("Serving files from %s on port: %s\n", root, port)
     log.Fatal(server.ListenAndServe())
+}
+
+func checkHealth(w http.ResponseWriter, req *http.Request) {
+    w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(http.StatusText(http.StatusOK)))
 }
